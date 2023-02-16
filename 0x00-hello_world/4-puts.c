fb7fa8cb2854ad430a38249b52a97bf2f8d7b0da
dominic6809 <muuomusyoki018@gmail.com>
2023-02-16 11:07:51 -0800

    printf function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - A program code that prints a line with puts function when compiled
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle);
-	return (0);       	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);       	
 }	
