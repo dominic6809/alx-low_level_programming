bc8182a463ab2b90a861142a4fb0b186ec77180f
dominic6809 <muuomusyoki018@gmail.com>
2023-02-16 12:12:58 -0800

    c task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - C program that prints a line to the standard error
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (0);
+}
