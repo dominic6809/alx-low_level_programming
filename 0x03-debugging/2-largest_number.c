52bc2f971a99ed618b1abf1098c612f5100731ab
dominic6809 <muuomusyoki018@gmail.com>
2023-02-22 10:43:11 -0800

    print largest 3 number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,16 @@
 
 int largest_number(int a, int b, int c)
 {
-    int largest;
+	int largest = b;
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
+	if (a > b)
+	{
+		largest = a;
+	}
+	else if (c > b)
+	{
+		largest = c;
+	}
 
-    return (largest);
+	return (largest);
 }
