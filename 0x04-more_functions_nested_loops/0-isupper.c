1bcbaee2e705ce1da577796fc2c88c01a3a4e492
dominic6809 <muuomusyoki018@gmail.com>
2023-02-23 08:53:18 -0800

    uppercase character

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isupper - checks if a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
+
+int _isupper(int a)
+{
+	if (a >= 65 && a <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
