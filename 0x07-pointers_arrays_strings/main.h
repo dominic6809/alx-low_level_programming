6a1f0a3356a2979fc4f6f7e2872a0c6b3e2e99e9
dominic6809 <muuomusyoki018@gmail.com>
2023-03-06 10:59:12 -0800

    fill memory with const byte

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,15 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Firdaus
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x07-pointers_arrays_strings directory.
- */
-
-#include <stddef.h>
-
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
 void *_memcpy(void *dest, const void *src, size_t n);
@@ -21,4 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif
+#endif /* MAIN_H */
