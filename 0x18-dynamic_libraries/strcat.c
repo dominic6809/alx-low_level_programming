9400544992b586bf7093f9f334be05d1deb8ecc4
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-25 19:26:49 +0300

    function to concat two strings

--- 0x18-dynamic_libraries/strcat.c
+++ 0x18-dynamic_libraries/strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings
- * @dest: A pointer to the string to be concatenated upon.
- * @src: The source string to be appended to @dest.
- * Return: A pointer to the destination string @dest.
+ * _strcat - Concats two strings
+ * @dest: pointer to the string to be concatenated upon.
+ * @src: source str to be appended to @dest.
+ * Return: (pointer) to the destination str @dest.
  */
 char *_strcat(char *dest, char *src)
 {
