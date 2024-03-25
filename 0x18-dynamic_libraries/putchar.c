123ad7cfda1678741102a1f25137238c19afa2fc
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-25 18:33:05 +0300

    Update putchar.c

--- 0x18-dynamic_libraries/putchar.c
+++ 0x18-dynamic_libraries/putchar.c
@@ -3,7 +3,9 @@
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
- * Return: On success 1, failure -1
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
