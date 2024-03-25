82fbe025644d61c04c953cfa9c0f8f9ac5c2eaeb
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-03-25 19:24:01 +0300

    Fills a memory area with a constant byte

--- 0x18-dynamic_libraries/memset.c
+++ 0x18-dynamic_libraries/memset.c
@@ -2,12 +2,11 @@
 
 /**
  * _memset - Fills a memory area with a constant byte
- * @s: Pointer to the memory area to fill.
- * @b: The constant byte to be used.
- * @n: Maximum number of bytes to fill.
- * Description: Fills the first @n bytes of the memory area pointed to by @s
- * with the constant byte @b.
- * Return: Pointer to the memory area @s.
+ * @s: pointer to the memory address area to fill.
+ * @b: constant byte to be used.
+ * @n: maximum number of bytes to fill.
+ * Desc: Fills the first @n bytes of the memory area pointed to by @s with @b.
+ * Return: (Pointer) to the mem area @s.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
