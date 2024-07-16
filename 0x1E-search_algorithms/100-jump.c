c0dde878dd4ea94ac18b2308881cd95d0c9d1b39
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-16 17:41:38 +0300

    search a value in a sorted singly linked list

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -4,53 +4,42 @@
 
 /**
  * jump_search - Searches for a value in a sorted singly linked list of integers
- *              using the Jump search algorithm.
+ *               using the Jump search algorithm.
  *
- * @list: A pointer to the head of the list to search in.
- * @size: The number of nodes in the list.
+ * @array: A pointer to the first element of the array to search in.
+ * @size: The number of elements in the array.
  * @value: The value to search for.
- * Return: A pointer to the first node where the value is located.
- *         If the value is not found or if the list is NULL, return NULL.
+ * Return: The first index where the value is located, or -1 if not found.
  */
 int jump_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+    int index, i, curr, prev;
 
-    size_t curr;
-    size_t prev;
-    size_t i;
+    if (array == NULL || size == 0)
+        return (-1);
 
-    curr = sqrt(size);
-    prev = 0;
+    i = (int)sqrt((double)size);
+    curr = 0;
+    prev = index = 0;
 
-    while (array[prev] < value)
+    do {
+        printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+        if (array[index] == value)
+            return (index);
+        curr++;
+        prev = index;
+        index = curr * i;
+    } while (index < (int)size && array[index] < value);
+
+    printf("Value found between indexes [%d] and [%d]\n", prev, index);
+
+    for (; prev <= index && prev < (int)size; prev++)
     {
-        printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
-
-        if (prev + curr >= size)
-        {
-            for (i = prev; i < size; i++)
-                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-            break;
-        }
-
-        if (array[prev + curr] >= value)
-        {
-            for (i = prev; i <= prev + curr; i++)
-                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-            break;
-        }
-
-        prev += curr;
+        printf("Value checked array[%d] = [%d]\n", prev, array[prev]);
+        if (array[prev] == value)
+            return (prev);
     }
 
-    for (i = prev; i < size && i < prev + curr; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return i;
-    }
-
-    return -1;
+    return (-1);
 }
