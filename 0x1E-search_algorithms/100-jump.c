ba7b046157ff2e465b4f360229dc36e024783cd7
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-16 17:36:13 +0300

    jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -3,59 +3,54 @@
 #include "search_algos.h"
 
 /**
- * jump_list - Searches for a value in a sorted singly linked list of integers
+ * jump_search - Searches for a value in a sorted singly linked list of integers
  *              using the Jump search algorithm.
  *
  * @list: A pointer to the head of the list to search in.
  * @size: The number of nodes in the list.
- * @value: The value to search for
+ * @value: The value to search for.
  * Return: A pointer to the first node where the value is located.
  *         If the value is not found or if the list is NULL, return NULL.
  */
-listint_t *jump_list(listint_t *list, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-    size_t jump, prev, curr;
-    listint_t *start, *end;
-    
-    if (list == NULL || size == 0)
-    {
-        return (NULL);
-    }
-    
-    jump = sqrt(size);
-    start = list;
-    end = list;
-    
-    while (end->next != NULL && end->n < value)
-    {
-        printf("Value checked at index [%zu] = [%d]\n", prev, end->n);
+    if (array == NULL)
+        return -1;
 
-        prev += jump;
-        for (size_t i = 0; i < jump && end->next != NULL; i++)
+    size_t curr;
+    size_t prev;
+    size_t i;
+
+    curr = sqrt(size);
+    prev = 0;
+
+    while (array[prev] < value)
+    {
+        printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+
+        if (prev + curr >= size)
         {
-            end = end->next;
-        }
-        
-        printf("Value checked at index [%zu] = [%d]\n", prev, end->n);
-        
-        if (end->n >= value)
-        {
+            for (i = prev; i < size; i++)
+                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
             break;
         }
-    }
-    
-    listint_t *node = start;
-    while (node != NULL && node->n <= value)
-    {
-        printf("Value checked at index [%zu] = [%d]\n", prev, node->n);
 
-        if (node->n == value)
+        if (array[prev + curr] >= value)
         {
-            return node;
+            for (i = prev; i <= prev + curr; i++)
+                printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+            break;
         }
-        node = node->next;
-        prev++;
+
+        prev += curr;
     }
 
-    return (NULL);
+    for (i = prev; i < size && i < prev + curr; i++)
+    {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+        if (array[i] == value)
+            return i;
+    }
+
+    return -1;
 }
