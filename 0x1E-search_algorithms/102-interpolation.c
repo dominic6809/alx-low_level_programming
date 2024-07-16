8ede1ec29a5620593e4e9e9f58d74208b4163055
dominic6809 <muuomusyoki018@gmail.com>
2024-07-16 10:19:59 -0700

    exponential search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -5,37 +5,39 @@
 /**
  * interpolation_search - Searches for a value in a sorted array of integers
  * using the Interpolation search algorithm.
+ *
  * @array: A pointer to the first element of the array to search in.
- * @size: The number of elements in the array.
+ * @size:  The number of elements in the array.
  * @value: The value to search for.
+ *
  * Return: The first index where value is located, or -1 if not found.
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-    size_t low, high, pos;
-    double ft;
+	size_t low, high, pos;
+	double ft;
 
-    if (array == NULL || size == 0)
-        return (-1);
+	if (array == NULL || size == 0)
+		return (-1);
 
-    low = 0;
-    high = size - 1;
+	low = 0;
+	high = size - 1;
 
-    while (low <= high && value >= array[low] && value <= array[high])
-    {
-        ft = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
-        pos = (size_t)(low + ft);
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		ft = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + ft);
 
-        printf("Value checked array[%zu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%zu] = [%d]\n", pos, array[pos]);
 
-        if (array[pos] == value)
-            return ((int)pos);
+		if (array[pos] == value)
+			return ((int)pos);
 
-        if (array[pos] < value)
-            low = pos + 1;
-        else
-            high = pos - 1;
-    }
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
 
-    return (-1);
+	return (-1);
 }
