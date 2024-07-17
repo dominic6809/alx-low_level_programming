9a95b74fee4256d0bb97327ca27a00cc26f8f306
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-17 04:44:48 +0300

    linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,21 +11,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+	int val;
 
 	if (array == NULL)
 		return (-1);
 
-	for (; i < size; ++i)
+	for (val = 0; val < (int)size; val++)
 	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-
-		if (array[i] == value)
-			break;
+		printf("Value checked array[%u] = [%d]\n", val, array[val]);
+		if (value == array[val])
+			return (val);
 	}
-
-	if (i == size)
-		return (-1);
-
-	return (i);
+	return (-1);
 }
