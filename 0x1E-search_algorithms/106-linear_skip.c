ee7e636f4748fc54fc08380fe075ed3fbf5f3518
dominic6809 <muuomusyoki018@gmail.com>
2024-07-16 18:39:45 -0700

    linear skip search algorithm

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
+#include <math.h>
 #include "search_algos.h"
 
 /**
  * linear_skip - Searches for a value in a sorted skip list of integers
- *               using the express lane nodes to skip sections of the list.
+ *               using the Jump search algorithm.
  *
  * @list: A pointer to the head of the skip list to search in.
  * @value: The value to search for.
  * Return: A pointer to the first node where value is located,
- *         or NULL if value is not present in the list or if head is NULL.
+ *         or NULL if value is not present in list or if head is NULL.
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-	skiplist_t *node;
+	skiplist_t *nd;
 
 	if (list == NULL)
 		return (NULL);
 
-	node = list;
+	nd = list;
 
 	do {
-		list = node;
-		node = node->express;
-		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
-	} while (node->express && node->n < value);
+		list = nd;
+		nd = nd->express;
+		printf("Value checked at index [%d] = [%d]\n", (int)nd->index, nd->n);
+	} while (nd->express && nd->n < value);
 
-	if (node->express == NULL)
+	if (nd->express == NULL)
 	{
-		list = node;
-		while (node->next)
-			node = node->next;
+		list = nd;
+		while (nd->next)
+			nd = nd->next;
 	}
 
-	printf("Value found between indexes [%lu] and [%lu]\n"
-		       list->index, node->index);
+	printf("Value found between indexes [%d] and [%d]\n",
+	       (int)list->index, (int)nd->index);
 
-	while (list != node->next)
+	while (list != nd->next)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", list->index, list->n);
+		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
 		if (list->n == value)
 			return (list);
 		list = list->next;
